c4a2a5c9fb4d58bec071d1d8a775d852dfedd814
Oliver Jowett <oliver@mutability.co.uk>
2015-12-23 10:10:05 +0000

    Fix the upintheair URL.

--- public_html/script.js
+++ public_html/script.js
@@ -498,7 +498,7 @@ function initialize_map() {
         // fetch a json file from the API for the altitudes you want to see:
         //
         //  wget -O /usr/share/dump1090-mutability/html/upintheair.json \
-        //    'http://www.heywhatsthat.com/api/upintheair.json?view=XXXX&refraction=0.25&alts=3048,9144'
+        //    'http://www.heywhatsthat.com/api/upintheair.json?id=XXXX&refraction=0.25&alts=3048,9144'
         //
         // NB: altitudes are in _meters_, you can specify a list of altitudes
 
