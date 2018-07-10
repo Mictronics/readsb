fffd3c9913e3ddb1302cbe0c316e873515570cc0
Oliver Jowett <oliver@mutability.co.uk>
2018-07-10 23:38:47 +0100

    Bump TSV_VERSION due to 7x00 handling change

--- net_io.c
+++ net_io.c
@@ -1863,7 +1863,7 @@ __attribute__ ((format (printf,4,5))) static char *appendFATSV(char *p, char *en
 }
 
 #define TSV_MAX_PACKET_SIZE 600
-#define TSV_VERSION 2
+#define TSV_VERSION 3
 
 void writeFATSVHeader()
 {
