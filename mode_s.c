56aa2f7d1e5d52a2df684d3c4bdae7a2bd21f173
Oliver Jowett <oliver@mutability.co.uk>
2019-03-19 18:23:49 +0000

    Clarify some comments in v1 TSS

--- mode_s.c
+++ mode_s.c
@@ -1053,7 +1053,7 @@ static void decodeESTargetStatus(struct modesMessage *mm, int check_imf)
             // nothing
             break;
         }
-        // 10: target altitude type (ignored)
+        // 10: target altitude type (MSL or Baro, ignored)
         // 11: backward compatibility bit, always 0
         // 12-13: target alt capabilities (ignored)
         // 14-15: vertical mode
@@ -1081,7 +1081,7 @@ static void decodeESTargetStatus(struct modesMessage *mm, int check_imf)
             break;
         }
 
-        // 16-25: altitude
+        // 16-25: target altitude
         int alt = -1000 + 100 * getbits(me, 16, 25);
         switch (mm->nav.altitude_source) {
         case NAV_ALT_MCP:
@@ -1109,7 +1109,7 @@ static void decodeESTargetStatus(struct modesMessage *mm, int check_imf)
                 mm->nav.heading_type = HEADING_MAGNETIC_OR_TRUE;
             }
         }
-        // 38-39: horiontal mode
+        // 38-39: horizontal mode
         switch (getbits(me, 38, 39)) {
         case 1: // acquiring
         case 2: // maintaining
